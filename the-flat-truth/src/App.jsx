08eff9a205474d53a9c1a763e262a17c2f40c020
eoghan523 <eoghan523@gmail.com>
2024-12-08 22:49:36 +0000

    Fixed errors on app.jsx and syntax errors

--- the-flat-truth/src/App.jsx
+++ the-flat-truth/src/App.jsx
@@ -1,23 +1,25 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import loginPage from './pages/LoginPage';
-import Post from './pages/Post';
+import LoginPage from './pages/LoginPage';
+import Post from './pages/PostInDetail';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import FlatEarthFacts from './pages/FlatEarthFacts';
-import PageNotFound from './pages/FlatEarthFacts';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/post/:id" element={<Post />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/flatearthfacts" element={<FlatEarthFacts />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
--- the-flat-truth/src/pages/HomePage.jsx
+++ the-flat-truth/src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Grid, Paper, Typography } from '@mui/material';
 
 const HomePage = () => {
-    
+    return (
+        <div>
+            HomePage!
+        </div>
+    )
+
 }
+
+export default HomePage;
