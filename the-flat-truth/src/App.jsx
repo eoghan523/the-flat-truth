4dcc63e877d08d97df4489be44c8ac0cdb550860
eoghan523 <eoghan523@gmail.com>
2024-12-08 21:56:34 +0000

    Installed axios. scaffolded out a basic Router DOM function on app.jsx

--- the-flat-truth/src/App.jsx
+++ the-flat-truth/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import loginPage from './pages/LoginPage';
+import Post from './pages/Post';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import FlatEarthFacts from './pages/FlatEarthFacts';
+import PageNotFound from './pages/FlatEarthFacts';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
+export default App;
---
+++ the-flat-truth/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Grid, Paper, Typography } from '@mui/material';
+
+const HomePage = () => {
+    
+}
